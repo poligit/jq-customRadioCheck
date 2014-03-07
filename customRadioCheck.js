cc737378b6b9abf12c03fefb2c5f391f4b4b3717
poligit <gerhard@polisensky.at>
2014-03-07 13:05:11 +0100

    init
    from jQ 1.7 to jQ 1.3.2 includes disabled and checked attribute

--- customRadioCheck.js
+++ customRadioCheck.js
@@ -1,39 +1,67 @@
 /*
-* customRadioCheck: jQuery plguin for checkbox and radio replacement
-* Usage: $('input[type=checkbox], input[type=radio]').customRadioCheck();
-* Author: Cedric Ruiz
-* License: MIT
-*/
-;(function(){
-$.fn.customRadioCheck = function() {
+ * customRadioCheck: jQuery plguin for checkbox and radio replacement
+ * Usage: $('input[type=checkbox], input[type=radio]').customRadioCheck();
+ * Author: Cedric Ruiz
+ * License: MIT
+ */
+;
+(function () {
+    $.fn.customRadioCheck = function () {
 
-  return this.each(function() {
+        return this.each(function () {
 
-    var $this = $(this);
-    var $span = $('<span/>');
+            var $this = $(this);
+            var $span = $('<span/>');
 
-    $span.addClass('custom-'+ ($this.is(':checkbox') ? 'check' : 'radio'));
-    $this.is(':checked') && $span.addClass('checked'); // init
-    $span.insertAfter($this);
+            $span.addClass('custom-' + ($this.is(':checkbox') ? 'check' : 'radio'));
+            $this.is(':checked') && $span.addClass('checked'); // init
+            $span.insertAfter($this);
 
-    $this.parent('label').addClass('custom-label')
-      .attr('onclick', ''); // Fix clicking label in iOS
-    // hide by shifting left
-    $this.css({ position: 'absolute', left: '-9999px' });
 
-    // Events
-    $this.on({
-      change: function() {
-        if ($this.is(':radio')) {
-          $this.parent().siblings('label')
+            if ($this.attr('disabled') && $this.attr('checked'))
+                $span.addClass('disabled-checked');
+            else if ($this.attr('disabled'))
+                $span.addClass('disabled');
+
+
+            $this.parent('label').addClass('custom-label').attr('onclick', ''); // Fix clicking label in iOS
+            // hide by shifting left
+            $this.css({ position: 'absolute', left: '-9999px' });
+
+
+
+            // Events old style for jQuery 1.3.2
+            $(this).change(function () {
+                if ($this.is(':radio')) {
+                    $this.parent().siblings('label').find('.custom-radio').removeClass('checked');
+                }
+                $span.toggleClass('checked', $this.is(':checked'));
+            });
+
+            $(this).focus(function () {
+                $span.addClass('focus');
+            });
+
+            $(this).blur(function () {
+                $span.removeClass('focus');
+            });
+
+
+            // Events with on() ab jQuery 1.7
+            /*
+            $this.on({
+            change: function() {
+            if ($this.is(':radio')) {
+            $this.parent().siblings('label')
             .find('.custom-radio').removeClass('checked');
-        }
-        $span.toggleClass('checked', $this.is(':checked'));
-      },
-      focus: function() { $span.addClass('focus'); },
-      blur: function() { $span.removeClass('focus'); }
-    });
-  });
-};
+            }
+            $span.toggleClass('checked', $this.is(':checked'));
+            },
+            focus: function() { $span.addClass('focus'); },
+            blur: function() { $span.removeClass('focus'); }
+            });
+            */
+        });
+    };
 }());
 
